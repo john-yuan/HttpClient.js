2a95c20e3e2e89b75f01283962719902ebcfdddb
John Yuan <John.YCQ@outlook.com>
2019-07-18 11:34:28 +0800

    Update test case for compileURL.

--- test/web/spec/defaultOptions.spec.js
+++ test/web/spec/defaultOptions.spec.js
@@ -19,15 +19,17 @@ describe('defaultOptions', function () {
 
     it('should compile url correctly', function (done) {
         client.send({
-            url: '/api/all/echo/{ post.id }',
+            url: '/api/all/echo/{ post.id ? post.id : 0 }/{ typeof post.id }/{ message }/{- escapedMessage }',
             model: {
                 post: {
                     id: 1
-                }
+                },
+                message: '你好',
+                escapedMessage: '%E4%BD%A0%E5%A5%BD'
             }
         }, function (response) {
             var data = response.json();
-            expect(data.path).toBe('/api/all/echo/1');
+            expect(data.path).toBe('/api/all/echo/1/number/%E4%BD%A0%E5%A5%BD/%E4%BD%A0%E5%A5%BD');
             done();
         });
     });
